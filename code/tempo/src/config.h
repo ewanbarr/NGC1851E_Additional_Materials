415026f783045335671df405ce15b806bf89a9be
Arunima Dutta <aru.megha@gmail.com>
2023-05-31 13:40:08 +0200

    added tempo

---
+++ code/tempo/src/config.h
@@ -0,0 +1,4 @@
+c   config.h - values created by configure
+c   src/config.h.  Generated from config.h.in by configure.
+        logical ISBIGENDIAN
+        parameter (ISBIGENDIAN = .false.)
---
+++ code/tempo/src/version.h
@@ -0,0 +1,2 @@
+        character*32 VERSIONID
+        parameter (VERSIONID='2020-05-20 d313c22')
